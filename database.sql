0074e8ebbadd8c4ea7b5334116e65746c1b541a5
YovanHirales <yovanhirales@gmail.com>
2023-12-06 19:20:29 -0800

    Attempting again

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
 -- CREATE DATABASE wordlist;
 
--- CREATE TABLE words(
---     word_id SERIAL PRIMARY KEY,
---     word VARCHAR(255),
---     part_of_speech_1 VARCHAR(255),
---     definition_1 VARCHAR(255),
---     part_of_speech_2 VARCHAR(255),
---     definition_2 VARCHAR(255)
--- );
+CREATE TABLE words(
+    word_id SERIAL PRIMARY KEY,
+    word VARCHAR(255),
+    part_of_speech_1 VARCHAR(255),
+    definition_1 VARCHAR(255),
+    part_of_speech_2 VARCHAR(255),
+    definition_2 VARCHAR(255)
+);
