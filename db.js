bf7f9d9acdc73cb78af7bc3c479078f495c54f66
YovanHirales <yovanhirales@gmail.com>
2023-12-07 18:50:36 -0800

    Added ssl line back

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const devConfig = {
 
 const proConfig = {
 	connectionString: process.env.DATABASE_URL,
-	// ssl: { rejectUnauthorized: false },
+	ssl: { rejectUnauthorized: false },
 };
 
 const pool = new Pool(
--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.post('/words', async (req, res) => {
 			const json = await response.json();
 			//insert word and def into database
 			console.log(json);
+			console.log(json[0]);
 			await pool.query(
 				'INSERT INTO words(word, part_of_speech_1, definition_1, part_of_speech_2, definition_2) VALUES($1, $2, $3, $4, $5);',
 				[
